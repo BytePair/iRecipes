6394bddb4c656787dc40ab49a5310462d2695052
BytePair <bytepair@gmail.com>
2018-01-06 05:07:12 -0500

    Add Starter Pages and Set Tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { TabsPage } from '../pages/tabs/tabs';
+import { AddRecipePage } from "../pages/add-recipe/add-recipe";
+import { RecipesPage } from "../pages/recipes/recipes";
+import { ShoppingListPage } from "../pages/shopping-list/shopping-list";
+
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    AddRecipePage,
+    RecipesPage,
+    ShoppingListPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp, {
+      backButtonText: 'Back',
+      backButtonIcon: 'ios-arrow-back',
+      iconMode: 'md'
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    AddRecipePage,
+    RecipesPage,
+    ShoppingListPage,
     TabsPage
   ],
   providers: [
